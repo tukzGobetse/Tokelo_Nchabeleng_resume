b3b95c08538b22abcd322a967730e08c324bed02
Nchabeleng Tokelo <Tokelo>
2021-07-20 01:44:00 +0200

    Profile Completed Links fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
 $(document).ready(() => {
-    // typing text animation script
+
+    /*By CodingNepal -September 16, 2020
+     https://www.codingnepalweb.com/responsive-personal-portfolio-website/ 
+     */
+    
+     // typing text animation script
     let typed = new Typed(".typing", {
         strings: ["Telecommunications", "Software Dev", "Embedded Systems", "Machine Learning", "Robotics"],
         typeSpeed: 100,
